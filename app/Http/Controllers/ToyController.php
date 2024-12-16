22c064cd1a17a6b2fe1c5934cc058aa8676a1c8a
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-16 14:12:56 +0100

    feat:MVC & Tables

---
+++ app/Http/Controllers/ToyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Toy;
+use Illuminate\Http\Request;
+
+class ToyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Toy $toy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Toy $toy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Toy $toy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Toy $toy)
+    {
+        //
+    }
+}
