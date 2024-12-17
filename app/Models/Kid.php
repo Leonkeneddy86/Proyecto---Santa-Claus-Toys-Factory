f28eae5ee4df575fe38faa85fb9e3e77edec4097
Matias-Stadler <koutarcj@gmail.com>
2024-12-17 15:18:08 +0100

    fix:view

--- app/Models/Kid.php
+++ app/Models/Kid.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Testing\Fluent\Concerns\Has;
 
-class Feedback extends Model
+class Kid extends Model
 {
     use HasFactory;
 
