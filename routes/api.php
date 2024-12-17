d18cf0b78138278d88071602f14bbc8681d2c0bb
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-17 12:15:05 +0100

    feat:apiToyController added

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\Kid;
 use App\Http\Controllers\Api\Toy;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KidController;
+use App\Http\Controllers\ToyController;
 
 Route::get('/offers', [KidController::class, 'index'])->name('apindex');
 Route::post('/offers', [KidController::class, 'store'])->name('apistore');
 Route::get('/offers/{id}', [KidController::class, 'show'])->name('apishow');
 Route::put('/offers/{id}', [KidController::class, 'update'])->name('apiupdate');
 Route::delete('/offers/{id}', [KidController::class, 'destroy'])->name('apidestroy');
+
+
+Route::get('/offers/{offerId}/news', [ToyController::class, 'index'])->name('apindexNews');
+Route::post('/offers/{offerId}/news', [ToyController::class, 'store'])->name('apistoreNews');
+Route::get('/offers/{offerId}/news/{newsID}', [ToyController::class, 'show'])->name('apishowNews');
+Route::put('/offers/{offerId}/news/{newsID}', [ToyController::class, 'update'])->name('apiupdateNews');
+Route::delete('/offers/{offerId}/news/{newsId}', [ToyController::class, 'destroy'])->name('apidestroyNews');
