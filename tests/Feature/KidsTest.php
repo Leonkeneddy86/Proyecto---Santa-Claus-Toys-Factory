0452ec18c8e973fec61941034d5fac20764a2cc3
Matias-Stadler <koutarcj@gmail.com>
2024-12-17 14:36:39 +0100

    feat:test api

--- tests/Feature/KidsTest.php
+++ tests/Feature/KidsTest.php
@@ -2,19 +2,23 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Kid;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class KidsTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    public function test_ListOfKidsCanBeRead(){
+        $this->withoutExceptionHandling();
 
-        $response->assertStatus(200);
+        Kid::all();
+
+        $response = $this->get('/');
+        $response->assertStatus(200)
+                ->assertViewIs('home');
     }
 }
