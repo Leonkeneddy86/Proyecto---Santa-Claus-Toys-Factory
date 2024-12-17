d46c30bcb8e9fbe4bce205e371f724b03abfffe8
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-17 12:12:41 +0100

    feat:routes api Kid

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\Kid;
+use App\Http\Controllers\Api\Toy;
+use App\Http\Controllers\KidController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/offers', [KidController::class, 'index'])->name('apindex');
+Route::post('/offers', [KidController::class, 'store'])->name('apistore');
+Route::get('/offers/{id}', [KidController::class, 'show'])->name('apishow');
+Route::put('/offers/{id}', [KidController::class, 'update'])->name('apiupdate');
+Route::delete('/offers/{id}', [KidController::class, 'destroy'])->name('apidestroy');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KidController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [KidController::class, 'index'])->name('index');
+Route::get('/offers/{id}', [KidController::class, 'show'])->name('show');
+    
