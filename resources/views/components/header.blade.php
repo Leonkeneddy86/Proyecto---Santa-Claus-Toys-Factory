ebf4aa650acc831b34c7363bf6e84af25407ed45
mrene42 <mariarenebarco.969@gmail.com>
2024-12-18 09:27:46 +0100

    fix: table in elf.blade.php and header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
 <header>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img src="{{asset('img/logoHeader.png')}}" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            <a href="" class="flex items-center">
+                <img src="{{asset('img/logoHeader.png')}}" class="mr-3 h-6 sm:h-9" alt="headerLogo" />
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Santa Claus</span>
             </a>
         </div>
--- resources/views/elf.blade.php
+++ resources/views/elf.blade.php
@@ -18,7 +18,7 @@
             @foreach ($toys as $toy)
               <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{$kids->id}}
+                    {{$toys->id}}
                     </th>
                     <td class="px-6 py-4">{{$toys->id}}</td>
                     <td class="px-6 py-4">{{$toys->name}}</td>
