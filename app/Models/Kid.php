de382b5a7210b185a0284bf7408ce91ffceb3863
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-18 09:26:21 +0100

    fix:Models

--- app/Models/Kid.php
+++ app/Models/Kid.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Testing\Fluent\Concerns\Has;
 
-class Feedback extends Model
+class Kid extends Model
 {
     use HasFactory;
 
@@ -22,6 +22,6 @@ class Feedback extends Model
 
     public function Toy()
     {
-        return $this->belongsTo(Toy::class);
+        return $this->hasMany(Toy::class);
     }
 }
